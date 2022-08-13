6d1c2978025ccc86cdfb184584b48d4b7d0da18b
Kris Marshall <rflctnofu@hotmail.com>
2022-08-13 17:25:29 -0400

    working on controllers

--- models/User.js
+++ models/User.js
@@ -5,7 +5,12 @@ const userSchema = new Schema(
     {
         username: { type: String, require: true, unique: true, trim: true },
         email: { type: String, require: true, unique: true, match: [/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/] },
-        thoughts: [Thought],
+        thoughts: [
+            {
+                type: Schema.Types.ObjectId,
+                ref: 'Thought',
+            },
+        ],
         friends: [User],
     },
     {
