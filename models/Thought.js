8832f70dd13e67a0f9ef5e77a070f3796c2fb406
Kris Marshall <rflctnofu@hotmail.com>
2022-08-16 16:07:24 -0400

    cleaned up commented out code

--- models/Thought.js
+++ models/Thought.js
@@ -2,27 +2,6 @@ const { Schema, model } = require('mongoose');
 const date = require('date-and-time');
 const Reaction = require('./Reaction');
 
-// const reactionSchema = new Schema(
-//     {
-//         reactionId: { type: Schema.Types.ObjectId, ref: 'User' },
-//         reactionBody: { type: String, require: true, maxLength: 280 },
-//         username: { type: String, require: true },
-//         createdAt: {
-//             type: Date,
-//             default: Date.now,
-//             get: createdAtVal => date.format(createdAtVal, 'MM/DD/YYYY')
-//         }
-//     },
-//     {
-//         toJSON: {
-//             getters: true,
-//         }
-//     },
-//     {
-//         timestamps: true,
-//     }
-// )
-
 const thoughtSchema = new Schema(
     {
         thoughtText: {
