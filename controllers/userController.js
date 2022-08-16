c3540b6b1ce1b815644b74f2105d684091b02209
Kris Marshall <rflctnofu@hotmail.com>
2022-08-16 09:44:01 -0400

    fixed populating json results

--- controllers/userController.js
+++ controllers/userController.js
@@ -10,6 +10,8 @@ module.exports = {
     getSingleUser(req, res) {
         User.findOne({ _id: req.params.userId })
             .select('-__v')
+            .populate('thoughts')
+            .populate('friends')
             .then((user) =>
                 !user
                     ? res.status(404).json({ message: 'No user found with this ID' })
